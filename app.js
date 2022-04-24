9a88c97976194b4de8b93ffb37915b7cb3fc533b
EddYam <edder_yamir@hotmail.com>
2022-04-24 15:48:13 -0500

    1. Agregando archivo app.js y creando app de express

---
+++ app.js
@@ -0,0 +1,16 @@
+//Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+// Puerto en que vamos a ver la app: localhost:3000
+const port = 3000
+
+// path inicial, respondera a la url localhost:3000 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Inicializa la app 
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
